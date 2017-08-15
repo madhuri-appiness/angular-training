ec45efced7b0c33bea9799437c5ee078338e1822
prashant-github <prashant.github@gmail.com>
2017-08-15 21:31:01 +0530

    validation

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var app = angular.module('app', []);
 app.controller("main", function ($scope) {
     $scope.name = "Hello World !";
-    $scope.login = function (username, password) {
-        if (username == "" && password == "") {
-            alert("Enter username and password");
+       $scope.email = "";
+        $scope.password = "";
+    $scope.login = function(email, password) {
+     
+        $scope.regux = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ($scope.email == "") {
+           alert("please enter email");
         }
+        else if(!$scope.regux.test(email)) {
+            alert("Please enter valid email")
+        }
+        else if($scope.password == ""){
+            alert("enter password");
+        }
+        else{
+            alert("Thank you");
+        }
+ 
     }
-    $scope.da = "daad";
     $scope.limit = 140;
-
-    $scope.twitter = function (d) {
-        if ($scope.limit <= d.length) {
-
-        } else {
-            $scope.limit = $scope.limit - d.length;
+    $scope.showval = true;
+    $scope.show = function(){
+        if($scope.showval == true){
+            $scope.showval = false;
         }
-
+        else{
+            $scope.showval = true;
+        }
     }
+
 });

55f0b833e93418c37fca61c4fa98d4e7f50acfea
madhuri-appiness <madhuridp298@gmail.com>
2017-12-18 00:15:14 +0530

    assignment one

--- angular-assignment/controller/maincontroller.js
+++ angular-assignment/controller/maincontroller.js
@@ -3,6 +3,7 @@ var app = angular.module("app",[]);
 app.controller('apartment', function($scope){
     
   $scope.arr = [{
+      id : 1,
     input1 : $scope.input1,
     submenu : [{
         name: "",
@@ -10,31 +11,33 @@ app.controller('apartment', function($scope){
     }]
   }];
   
-  $scope.addApartment = function(){
+  $scope.addApartment = function(index_val){
+
     var id_value = $scope.arr[$scope.arr.length-1].submenu.length+1;
     console.log("clicked");
     console.log($scope.arr[$scope.arr.length-1].submenu);
-    $scope.arr[$scope.arr.length-1].submenu.push({"name":'',"id":'plot'+id_value});
-    console.log($scope.arr);
+    $scope.arr[index_val].submenu.push({"name":'',"id":'plot'+id_value});
+   
   };
     
   //copy apartment
   $scope.copy = function(){
-
+    var arr_id = $scope.arr.length + 1;
     $scope.obj_Val = {
-     
+     id : arr_id,
      input1 : $scope.arr[$scope.arr.length - 1].input1,
      submenu: $scope.copyObj($scope.arr[$scope.arr.length - 1].submenu)
    
     }
 
     $scope.arr.push($scope.obj_Val); 
-
+    console.log($scope.arr);
   };
 
-  $scope.deleteSubApart = function(item){
-    if($scope.arr[$scope.arr.length - 1].submenu.length > 1){
-      $scope.arr[$scope.arr.length - 1].submenu.splice(item,1);
+  $scope.deleteSubApart = function(arr_index, subindex){
+    console.log(arr_index, subindex)
+    if($scope.arr[arr_index].submenu.length >= 0){
+      $scope.arr[arr_index].submenu.splice(subindex, 1);
     }
   }
    
